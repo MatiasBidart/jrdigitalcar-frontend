85c76c910860b66c96b1d08203e144bc772266cd
Matias Bidart <matiasezequielbidart@gmail.com>
2025-12-09 16:27:11 -0300

    first commit/ERROR #BlogList

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,15 +1,16 @@
 "use client";
 
 import { BlogList } from '@/components/BlogList';
-import { BlogPost } from '@/types/blog';
-import { BlogService } from '@/services/blogService';
+// import { BlogPost } from '@/types/blog';
+// import { BlogService } from '@/services/blogService';
 
 export default function BlogPage() {
-  const posts: BlogPost[] = BlogService.getAllPosts();
+  // const posts: BlogPost[] = BlogService.getAllPosts();
 
   return (
     <div>
-      <BlogList posts={posts} />
+      {/* <BlogList posts={posts} /> */}
+      <BlogList/>
     </div>
   );
 }
