28dcf163c320eb287a461ed557d82fc731a0ad4b
Matias Bidart <matiasezequielbidart@gmail.com>
2025-12-12 14:47:22 -0300

    second commit/motion-ERROR-0.5

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import FixedButton from "@/components/FixedButton";
+import Script from "next/script";
 
 export const metadata = {
   title: {
@@ -20,6 +21,19 @@ export default function RootLayout({
   <html lang="es">
     <head>
         <meta name="google-site-verification" content="EHnJbwM_DP1rgFP5Oj3ieS9V1CvihDk1ScnWzrYBvj8" />
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-996575528"
+          strategy="afterInteractive"
+        />
+        <Script id="google-ads-tag" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-996575528');
+          `}
+        </Script>
       </head>
       <body>
         <div className="min-h-screen flex flex-col">
